429af2eee94f8822484098b47316c86432b8cefa
Darryl Crockett <darryl_crockett1@homedepot.com>
2023-05-02 08:07:20 -0400

    updated gitIgnore

--- next.config.js
+++ next.config.js
@@ -2,10 +2,7 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  // output: 'export',
+  output: 'export',
 }
 
 module.exports = nextConfig
